43efbf611e004b0b6c908d2110332404319aaffe
B Shadab Khan <shadabkhanbenki@gmail.com>
2024-10-25 15:15:29 +0530

    Update gemini_ai.py

--- gemini_ai.py
+++ gemini_ai.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("streamlit application")
-for i in range(10):
-            st.write(i)
+a=st.inpu("enter input")
+# for i in range(a):
+#             st.write(i)
             
