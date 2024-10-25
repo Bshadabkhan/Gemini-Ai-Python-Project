cc2bb5bf15bfbb3110c4d9f3167df4256b40f548
B Shadab Khan <shadabkhanbenki@gmail.com>
2024-10-25 15:11:04 +0530

    Update gemini_ai.py

--- gemini_ai.py
+++ gemini_ai.py
@@ -1,10 +1,3 @@
-# from gemini import GenerativeModel
-import google.generativeai as genai
-import os
+import streamlit as st
 
-genai.configure(api_key="AIzaSyAH7Mv_GEHx7nGj_-SmLGI6UaI_z3NlKEE")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("give me a story about laptop", stream=True)
-for chunk in response:
-    print(chunk.text)
-    print("_" * 80)
+st.title("streamlit application")
